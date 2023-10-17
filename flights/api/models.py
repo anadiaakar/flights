354e94474ca906140a9f105aa24cc82b9cdb47c9
Anadi <anadiaakar@gmail.com>
2023-10-17 14:35:27 +0530

    Chnages made

--- flights/api/models.py
+++ flights/api/models.py
@@ -19,3 +19,19 @@ class FlightDetails(models.Model):
 		return self.number
 
 
+class Passenger(models.Model):
+	Passenger_name = models.CharField(max_length=100)
+	passenger_passport_id = models.CharField(max_length=10,unique=True)
+	passenger_gender = models.CharField(max_length=2)
+	passenger_contact = models.CharField(max_length=100)
+ 
+class BookingDetails(models.Model):
+	number = models.CharField(max_length=100)
+	passenger = models.ForeignKey(Passenger, related_name='pas', on_delete=models.CASCADE)
+	booking_confirmation_time = models.DateTimeField()
+	booking_status = models.Boolean()
+	flight = models.ForeignKey(FlightDetails, related_name='arr', on_delete=models.CASCADE)
+ 
+	def __str__(self):
+		return self.number
+
