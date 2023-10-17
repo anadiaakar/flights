29fd3db280325d24d18e7fdd98f8539019f730ba
Anadi <anadiaakar@gmail.com>
2023-10-17 15:10:37 +0530

    Views added

--- flights/api/urls.py
+++ flights/api/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('update-flight/<int:id>' , views.Update.as_view()),
     path('flight-plan' , views.FlightPlan.as_view()),
     path('flight-find' , views.FindPlan.as_view()),
-    
+    path('passenger-create' , views.AddPassenger.as_view()),
+
      
 ]
